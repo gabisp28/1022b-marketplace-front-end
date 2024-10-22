7ab5b3b5f1a17a9c7da1b6f51ff11fc8e29e8749
Gabi <gabriela.palma@estudante.ifms.edu.br>
2024-10-22 07:37:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,32 @@ type ProdutoType = {
   descricao:string,
   preco:string,
   imagem:string
+};
+
+type UsuarioType = {
+  id:number,
+  nome:string,
+  email:string,
+  created_at:string,
+  updated_at:string
 }
+
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  const [usuarios, setUsuarios] = useState<UsuarioType[]>([])
   //useEffect(O Que fazer, Quando fazer)
   useEffect(()=>{
     fetch("https://one022b-marketplace-djsh.onrender.com/produtos")
     .then(resposta=>resposta.json())
     .then(dados=>setProdutos(dados))
+  },[]); 
+
+  useEffect(()=>{
+    fetch("https://one022b-marketplace-djsh.onrender.com/usuarios")
+    .then(resposta=>resposta.json())
+    .then(dados=>setUsuarios(dados))
   },[])
+
 
   return (
     <>
@@ -24,15 +41,28 @@ function App() {
       return(
         <div key={prod.id}className='produto-item'>
           <h1>{prod.nome}</h1>
-          <img src={prod.imagem} alt="Imagem do Celular" />
+          <img src={prod.imagem} alt="Imagem do celular" />
           <p>{prod.preco}</p>
           <p>{prod.descricao}</p>
 
         </div>
      ) 
     })}
-    </div>
 
+    </div>
+     <div className='container-usuarios'>
+      {usuarios.map(usua=>{
+        return(
+          <div key={usua.id}className='usuario-item'>
+            <h1>{usua.nome}</h1>
+            <p>{usua.email}</p>
+            <p>{usua.created_at}</p>
+            <p>{usua.updated_at}</p>
+          </div>
+        )
+      })}
+
+     </div>
     </>
   )
 }
